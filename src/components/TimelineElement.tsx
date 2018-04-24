01494b4d293a870119e4e70b0b3b9acd9a1e8704
Alex Wild <alex.j.wild@gmail.com>
2018-04-24 16:42:50 -0700

    Add projects and education to the timeline

--- src/components/TimelineElement.tsx
+++ src/components/TimelineElement.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import { VerticalTimelineElement } from 'react-vertical-timeline-component'
-import { Work as WorkIcon } from '@material-ui/icons'
+import {
+  Code as CodeIcon,
+  School as SchoolIcon,
+  Work as WorkIcon,
+} from '@material-ui/icons'
 
 interface TimelineComponentProps {
   element: {
@@ -12,21 +16,56 @@ interface TimelineComponentProps {
   }
 }
 
+const educationIconProps = {
+  icon: <SchoolIcon />,
+  iconStyle: {
+    background: 'orange',
+    color: '#fff',
+  },
+}
+
+const projectIconProps = {
+  icon: <CodeIcon />,
+  iconStyle: {
+    background: 'green',
+    color: '#fff',
+  },
+}
+
+const workIconProps = {
+  icon: <WorkIcon />,
+  iconStyle: {
+    background: 'blue',
+    color: '#fff',
+  },
+}
+
 export default ({
   element: { date, type, title, subtitle, description },
 }: TimelineComponentProps) => {
-  const workProps = {
-    icon: <WorkIcon />,
-    iconStyle: {
-      background: 'rgb(33, 150, 243)',
-      color: '#fff',
-    },
+  const iconProps = {}
+
+  switch (type) {
+    case 'education': {
+      Object.assign(iconProps, educationIconProps)
+      break
+    }
+    case 'project': {
+      Object.assign(iconProps, projectIconProps)
+      break
+    }
+    case 'work': {
+      Object.assign(iconProps, workIconProps)
+      break
+    }
   }
 
   return (
-    <VerticalTimelineElement date={date} {...workProps}>
+    <VerticalTimelineElement date={date} {...iconProps}>
       <h3 className="vertical-timeline-element-title">{title}</h3>
-      <h4 className="vertical-timeline-element-subtitle">{subtitle}</h4>
+      {subtitle && (
+        <h4 className="vertical-timeline-element-subtitle">{subtitle}</h4>
+      )}
       <p>{description}</p>
     </VerticalTimelineElement>
   )
