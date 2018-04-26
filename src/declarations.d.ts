2755a1e3c8a79ec16a242b4df2128006971e65ca
Alex Wild <alex.j.wild@gmail.com>
2018-04-25 22:02:41 -0700

    Fix SVG import lint warning

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,7 @@
 declare module 'react-vertical-timeline-component'
 declare module 'typography-breakpoint-constants'
 declare const graphql: (query: TemplateStringsArray) => void
+declare module '*.svg' {
+  const content: any
+  export default content
+}
