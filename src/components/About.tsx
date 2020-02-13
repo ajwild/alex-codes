ada1b0273ce900a9be2dd143f96f94ab2c09e62b
Alex Wild <alex.j.wild@gmail.com>
2020-02-13 15:54:24 +0900

    Update the About component content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,13 +10,22 @@ function AboutComponent(): React.ReactElement {
       <h3>Hello, how do you do?</h3>
       <p>
         My name is Alex Wild and I am a full stack JavaScript developer living
-        in Vancouver, BC. I enjoy working as a full stack developer, from
+        in Zürich, Switzerland. I enjoy working as a full stack developer, from
         provisioning servers all the way up to getting the user interface
-        pixel-perfect, as the variety of work keeps me motivated and there is
-        always something new to learn. For front-end code I have recently been
-        using Vue.js and React, and I have spent a number of years working with
-        AngularJS. For back-end code I often use Node.js with the Express or
-        Serverless frameworks.
+        pixel-perfect, as the variety of work keeps me motivated. I am very keen
+        to continue working with JavaScript, either building data-rich
+        interactive web apps or APIs which solve complex problems, as the
+        ecosystem is constantly evolving so there is always something new to
+        learn.
+      </p>
+      <p>
+        For front-end code I have recently been using Vue.js and React, and I
+        have spent a number of years working with AngularJS. For back-end code I
+        often use Node.js with the Express, Koa or Serverless frameworks. I have
+        experience using Amazon Web Services and Google Cloud Platform, as well
+        as working with Docker containers and provisioning bare-metal servers. I
+        am also familiar with a variety of databases and file storage
+        technologies.
       </p>
       <p>
         If you&apos;d like to get in touch you can connect with me on{' '}
